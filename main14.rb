36b2f5a80dc8bbd063f607344ff71a5f7cb468a1
Hossein <hosseinsalmani1373@gmail.com>
2021-06-05 21:06:42 +0430

    Arrays

---
+++ main14.rb
@@ -0,0 +1,23 @@
+# آرایه ها در روبی
+
+# myarray = Array['a','b','c']
+# puts myarray
+#
+# puts "#{myarray}"
+#
+# a = Array(0..9)
+# puts a
+# puts "#{a}"
+
+# x = Array.new(20)
+# puts x
+# puts "#{x}"
+
+name = Array.new(4, "Ali")
+puts "#{name}"
+str = ['a', 'b', 'c', 'd', 'e', 'f']
+puts str[1]
+puts str[0]
+puts str[-1]
+puts "=========================="
+puts str[1,4]
